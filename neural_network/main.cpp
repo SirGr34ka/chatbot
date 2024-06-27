0094101c0231190f6331b3a0cb62db50350a9956
SirGr34ka <osipov.d.yr@gmail.com>
2024-06-28 00:48:12 +0700

    First example without training

--- neural_network/main.cpp
+++ neural_network/main.cpp
@@ -1,45 +1,60 @@
 #include <iostream>
+#include <math.h>
 
 #include "matrix.h"
 
+double sigmoid( const double value )
+{
+    return 1 / ( 1 + exp( -value ) );
+}
+
 int main()
 {
-    size_t rows , columns;
+    // Input neurons
+    Matrix input ( 1 , 2 );
 
-    // Создание и задание первой матрицы
-    std::cout << "Input first matrix rows: ";
-    std::cin >> rows;
-    std::cout << "Input first matrix columns: ";
-    std::cin >> columns;
+    input.matrix[0][0] = 1;
+    input.matrix[0][1] = 0;
 
-    Matrix first ( rows , columns );
+    std::cout << "Input neurons:" << std::endl;
+    std::cout << input;
 
-    std::cout << "Input first matrix: ";
-    std::cin >> first;
+    // Input -> Hidden weights
+    Matrix input_hidden_weights ( 2 , 2 );
 
-    std::cout << "First matrix:" << std::endl;
-    std::cout << first;
+    input_hidden_weights.matrix[0][0] = 0.45;
+    input_hidden_weights.matrix[0][1] = 0.78;
+    input_hidden_weights.matrix[1][0] = -0.12;
+    input_hidden_weights.matrix[1][1] = 0.13;
 
-    // Создание и задание второй матрицы
-    std::cout << "Input second matrix rows: ";
-    std::cin >> rows;
-    std::cout << "Input second matrix columns: ";
-    std::cin >> columns;
+    // Hidden neurons
+    Matrix hidden = input * input_hidden_weights;
 
-    Matrix second ( rows , columns );
+    // Activation function
+    hidden.matrix[0][0] = sigmoid( hidden.matrix[0][0] );
+    hidden.matrix[0][1] = sigmoid( hidden.matrix[0][1] );
 
-    std::cout << "Input second matrix: ";
-    std::cin >> second;
+    std::cout << "Hidden neurons:" << std::endl;
+    std::cout << hidden;
 
-    std::cout << "Second matrix:" << std::endl;
-    std::cout << second;
+    // Hidden -> Output weights
+    Matrix hidden_output_weights ( 2 , 1 );
 
-    // Создание и задание результирующей матрицы
+    hidden_output_weights.matrix[0][0] = 1.5;
+    hidden_output_weights.matrix[1][0] = -2.3;
 
-    Matrix result = first * second;
+    // Output neuron
+    Matrix output = hidden * hidden_output_weights;
 
-    std::cout << "Result:" << std::endl;
-    std::cout << result;
+    // Activation fuction
+    output.matrix[0][0] = sigmoid( output.matrix[0][0] );
+
+    std::cout << "Output neuron:" << std::endl;
+    std::cout << output;
+
+    // Error
+    std::cout << "Error:" << std::endl;
+    std::cout << pow( 1 - output.matrix[0][0] , 2 ) << std::endl;
 
     return 0;
 }
--- neural_network/matrix.cpp
+++ neural_network/matrix.cpp
@@ -1,198 +1,49 @@
 #include <iostream>
 #include "matrix.h"
 
-Matrix::Matrix()
-{
-    rows = 2;
-    columns = 2;
-
-    matrix = new int* [rows];
-        
-    for (size_t i = 0; i < rows; ++i)
-    {
-        matrix[i] = new int [columns]{};
-    }
-}
-
-Matrix::Matrix(const int& m, const int& n)
+Matrix::Matrix( const size_t& m, const size_t& n )
 {
     rows = m;
     columns = n;
 
-    matrix = new int* [rows];
+    matrix = new double* [ rows ];
         
-    for (size_t i = 0; i < rows; ++i)
+    for ( size_t i = 0 ; i < rows ; ++i )
     {
-        matrix[i] = new int [columns]{};
+        matrix[i] = new double [ columns ]{};
     }
 }
 
-Matrix::Matrix(Matrix& copied_matrix)
+Matrix::Matrix( Matrix& copied_matrix )
 {
-    rows = copied_matrix.getRows();
-    columns = copied_matrix.getColumns();
+    rows = copied_matrix.rows;
+    columns = copied_matrix.columns;
 
-    matrix = new int* [rows];
-        
-    for (size_t i = 0; i < rows; ++i)
+    matrix = new double* [ rows ];
+    
+    for ( size_t i = 0 ; i < rows ; ++i )
     {
-        matrix[i] = new int [columns];
+        matrix[i] = new double [ columns ];
     }
 
     *this = copied_matrix;
 }
 
-int** Matrix::getMatrix()
-{
-    return matrix;
-}
-
-int Matrix::getRows()
-{
-    return rows;
-}
-
-int Matrix::getColumns()
-{
-    return columns;
-}
-
-int Matrix::max()
+Matrix Matrix::operator * ( Matrix& right_matrix )
 {
     const size_t m = rows;
-    const size_t n = columns;
-    int max_num = **matrix;
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* row = matrix[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            max_num = row[j] > max_num ? row[j]: max_num;
-        }
-    }
-
-    return max_num;
-}
-
-int Matrix::dotProduct(Matrix& input_matrix, const size_t& row_offset = 0, const size_t& column_offset = 0)
-{
-    int res = 0;
-
-    const size_t m = input_matrix.getRows();
-    const size_t n = input_matrix.getColumns();
-    int** input_matrix_ptr = input_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* row = matrix[i + row_offset];
-        int* input_row = input_matrix_ptr[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            res += row[j + column_offset] * input_row[j];
-        }
-    }
-
-    return res;
-}
-
-Matrix Matrix::getPatch(Matrix& input_matrix, const size_t& row_offset = 0, const size_t& column_offset = 0)
-{
-    const size_t m = rows;
-    const size_t n = columns;
-    int** input_matrix_ptr = input_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* row = matrix[i];
-        int* input_row = input_matrix_ptr[i + row_offset];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            row[j] = input_row[j + column_offset];
-        }
-    }
-
-    return *this;
-}
-
-Matrix Matrix::operator + (Matrix right_matrix)
-{
-    const size_t m = rows;
-    const size_t n = columns;
-    int** right_matrix_ptr = right_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* right_row = right_matrix_ptr[i];
-        int* left_row = matrix[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            right_row[j] += left_row[j];
-        }
-    }
-
-    return right_matrix;
-}
-
-Matrix Matrix::operator - (Matrix right_matrix)
-{
-    const size_t m = rows;
-    const size_t n = columns;
-    int** right_matrix_ptr = right_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* right_row = right_matrix_ptr[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            right_row[j] = ~right_row[j] + 1;
-        }
-    }
-
-    return *this + right_matrix;
-}
-
-Matrix Matrix::operator * (const int& right_num)
-{
-    const size_t m = rows;
-    const size_t n = columns;
-
-    Matrix left_matrix = *this;
-    int** left_matrix_ptr = left_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* left_row = left_matrix_ptr[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            left_row[j] *= right_num;
-        }
-    }
-
-    return left_matrix;
-}
-
-Matrix Matrix::operator * (Matrix& right_matrix)
-{
-    const size_t m = rows;
-    const size_t n = right_matrix.getColumns();
+    const size_t n = right_matrix.columns;
     const size_t k = columns;
-    int** right_matrix_ptr = right_matrix.getMatrix();
+    double** right_matrix_ptr = right_matrix.matrix;
 
-    Matrix result_matrix(m, n);
-    int** result_matrix_ptr = result_matrix.getMatrix();
+    Matrix result_matrix( m , n );
+    double** result_matrix_ptr = result_matrix.matrix;
 
-    for (size_t i = 0; i < m; ++i)
+    for ( size_t i = 0 ; i < m ; ++i )
     {
-        for (size_t j = 0; j < n; ++j)
+        for ( size_t j = 0 ; j < n ; ++j )
         {
-            for (size_t s = 0; s < k; ++s)
+            for ( size_t s = 0 ; s < k ; ++s )
             {
                 result_matrix_ptr[i][j] += matrix[i][s] * right_matrix_ptr[s][j];
             }
@@ -202,18 +53,18 @@ Matrix Matrix::operator * (Matrix& right_matrix)
     return result_matrix;
 }
 
-Matrix& Matrix::operator = (Matrix& right_matrix)
+Matrix& Matrix::operator = ( Matrix& right_matrix )
 {
     const size_t m = rows;
     const size_t n = columns;
-    int** right_matrix_ptr = right_matrix.getMatrix();
+    double** right_matrix_ptr = right_matrix.matrix;
 
-    for (size_t i = 0; i < m; ++i)
+    for ( size_t i = 0 ; i < m ; ++i )
     {
-        int* left_row = matrix[i];
-        int* right_row = right_matrix_ptr[i];
+        double* left_row = matrix[i];
+        double* right_row = right_matrix_ptr[i];
 
-        for (size_t j = 0; j < n; ++j)
+        for ( size_t j = 0 ; j < n ; ++j )
         {
             left_row[j] = right_row[j];
         }
@@ -222,33 +73,9 @@ Matrix& Matrix::operator = (Matrix& right_matrix)
     return *this;
 }
 
-bool Matrix::operator == (Matrix& right_matrix)
-{
-    const size_t m = rows;
-    const size_t n = columns;
-
-    Matrix temp_matrix = *this - right_matrix;
-    int** temp_matrix_ptr = temp_matrix.getMatrix();
-
-    for (size_t i = 0; i < m; ++i)
-    {
-        int* temp_row = temp_matrix_ptr[i];
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            if (temp_row[j] != 0)
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
 Matrix::~Matrix()
 {
-    for (size_t i = 0; i < rows; ++i)
+    for ( size_t i = 0 ; i < rows ; ++i )
     {
         delete[] matrix[i];
     }
@@ -256,17 +83,17 @@ Matrix::~Matrix()
     delete[] matrix;
 }
 
-std::ostream& operator << (std::ostream &os, Matrix& matrix)
+std::ostream& operator << ( std::ostream &os , Matrix& matrix )
 {
-    const size_t m = matrix.getRows();
-    const size_t n = matrix.getColumns();
-    int** matrix_ptr = matrix.getMatrix();
+    const size_t m = matrix.rows;
+    const size_t n = matrix.columns;
+    double** matrix_ptr = matrix.matrix;
 
-    for (size_t i = 0; i < m; ++i)
+    for ( size_t i = 0 ; i < m ; ++i )
     {
-        int* row = matrix_ptr[i];
+        double* row = matrix_ptr[i];
 
-        for (size_t j = 0; j < n; ++j)
+        for ( size_t j = 0 ; j < n ; ++j )
         {
             os << row[j] << " ";
         }
@@ -279,15 +106,15 @@ std::ostream& operator << (std::ostream &os, Matrix& matrix)
 
 std::istream& operator >> (std::istream &is, Matrix& matrix)
 {
-    const size_t m = matrix.getRows();
-    const size_t n = matrix.getColumns();
-    int** matrix_ptr = matrix.getMatrix();
+    const size_t m = matrix.rows;
+    const size_t n = matrix.columns;
+    double** matrix_ptr = matrix.matrix;
 
-    for (size_t i = 0; i < m; ++i)
+    for ( size_t i = 0 ; i < m ; ++i )
     {
-        int* row = matrix_ptr[i];
+        double* row = matrix_ptr[i];
 
-        for (size_t j = 0; j < n; ++j)
+        for ( size_t j = 0 ; j < n ; ++j )
         {
             is >> row[j];
         }
