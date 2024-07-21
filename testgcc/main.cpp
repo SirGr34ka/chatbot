4f842f23534eba62b810f4b7431bb1647364795f
Константинов Лев <levkonst12@gmail.com>
2024-07-21 20:48:59 +0700

    Added the test-parser, work but so bad

---
+++ testgcc/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <curl/curl.h>
+#include "gumbo.h"
+#include "parser.h"
+#include "algorithm"
+#include "string"
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+  std::string arg = "iphone+14";
+  std::cout << arg;
+ 
+  std::string res = request(arg);
+  std::cout << scrape(res) << std::endl;
+ 
+  return EXIT_SUCCESS;
+}
